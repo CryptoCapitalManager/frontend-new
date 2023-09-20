93927564dcfadf7dee41bb77ee21005bb5886285
DzoniTheNick <nikolapetrovic12@gmail.com>
2023-09-20 10:55:04 +0200

    Add new approval panel

--- src/views/web/componenets/Dashboard/Invest/Invest.tsx
+++ src/views/web/componenets/Dashboard/Invest/Invest.tsx
@@ -1,4 +1,4 @@
-import { FC, useState } from "react";
+import { FC, useEffect, useRef, useState } from "react";
 import { ethers } from "ethers";
 
 import "./Invest.scss";
@@ -8,7 +8,10 @@ import trading_abi from "../../../../../abi/trading.json";
 import usdc from "../../../../../res/svg/usdc-balance.svg";
 
 import { investProps } from "../../../../../utils/props";
-import { TRADING_GOERLI_ADDRESS } from "../../../../../utils/const";
+import {
+    TRADING_GOERLI_ADDRESS,
+    USDC_APPROVAL_NEEDED,
+} from "../../../../../utils/const";
 
 const Invest: FC<investProps> = ({
     signer,
@@ -17,6 +20,7 @@ const Invest: FC<investProps> = ({
     setInvestVisible,
 }) => {
     const [investAmount, setInvestAmount] = useState<number>(0);
+    const [gotToApproval, setGoToApproval] = useState<boolean>(false);
 
     const deposit = async () => {
         const parsedAmount = ethers.parseEther(investAmount.toString());
@@ -29,76 +33,113 @@ const Invest: FC<investProps> = ({
 
         try {
             await trading.deposit(parsedAmount);
-        } catch (e) {
-            // TODO: Tell user all is good
-            console.log(e);
+        } catch (e: any) {
+            if (e.reason === USDC_APPROVAL_NEEDED) {
+                setGoToApproval(true);
+                return;
+                // GOTO: Approval and change screen
+            }
         }
 
         setInvestVisible(false);
     };
 
+    const approve = async () => {
+        setInvestVisible(false);
+    };
+
     return (
         <div className="invest-tab" style={{ height: `${dashboardHeight}px` }}>
-            <div className="container">
-                <div className="tab-header">
-                    <p id="header">Deposit</p>
-                    <div
-                        className="btn"
-                        onClick={() => {
-                            setInvestVisible(false);
-                        }}
-                    >
-                        X
-                    </div>
-                </div>
-
-                <div className="input">
-                    <div className="left">
-                        <input
-                            type="text"
-                            value={investAmount}
-                            onChange={(e) => {
-                                const result = Number(e.target.value);
-
-                                if (
-                                    !isNaN(result) &&
-                                    result >= 0 &&
-                                    result < Number.MAX_SAFE_INTEGER
-                                ) {
-                                    setInvestAmount(Number(e.target.value));
-                                }
+            {!gotToApproval ? (
+                <div className="initial-panel">
+                    <div className="tab-header">
+                        <p id="header">Deposit</p>
+                        <div
+                            className="btn"
+                            onClick={() => {
+                                setInvestVisible(false);
                             }}
-                            disabled={
-                                ethers.getBigInt(balanceUSDC) ===
-                                ethers.getBigInt(0)
-                            }
-                            style={{
-                                color:
-                                    +investAmount > 0 ? "rgba(0, 0, 0, 1)" : "",
-                            }}
-                        />
-                    </div>
-                    <div className="right">
-                        <img src={usdc} alt="USDC banner" />
-                        <div className="good-balance-text">
-                            <p id="balance">Balance: {balanceUSDC} </p>
-                            <p id="max">Max</p>
+                        >
+                            X
                         </div>
                     </div>
+                    <div className="input">
+                        <div className="left">
+                            <input
+                                type="text"
+                                value={investAmount}
+                                onChange={(e) => {
+                                    const result = Number(e.target.value);
+
+                                    if (
+                                        !isNaN(result) &&
+                                        result >= 0 &&
+                                        result < Number.MAX_SAFE_INTEGER
+                                    ) {
+                                        setInvestAmount(Number(e.target.value));
+                                    }
+                                }}
+                                disabled={
+                                    ethers.getBigInt(balanceUSDC) ===
+                                    ethers.getBigInt(0)
+                                }
+                                style={{
+                                    color:
+                                        +investAmount > 0
+                                            ? "rgba(0, 0, 0, 1)"
+                                            : "",
+                                }}
+                            />
+                        </div>
+                        <div className="right">
+                            <img src={usdc} alt="USDC banner" />
+                            <div className="good-balance-text">
+                                <p id="balance">Balance: {balanceUSDC} </p>
+                                <p id="max">Max</p>
+                            </div>
+                        </div>
+                    </div>
+                    {investAmount === 0 ? (
+                        <div className="empty-balance">Enter an amount</div>
+                    ) : (
+                        <div
+                            className="good-balance"
+                            onClick={() => {
+                                deposit();
+                            }}
+                        >
+                            Deposit to account
+                        </div>
+                    )}
                 </div>
-                {investAmount === 0 ? (
-                    <div className="empty-balance">Enter an amount</div>
-                ) : (
+            ) : (
+                <div className="approval-panel">
+                    <div className="tab-header">
+                        <div
+                            className="btn"
+                            onClick={() => {
+                                setInvestVisible(false);
+                            }}
+                        >
+                            X
+                        </div>
+                    </div>
+                    <div className="notification-field">
+                        <p>
+                            To enable the deposit in our smart contract, you
+                            need to approve the spending of USDC.
+                        </p>
+                    </div>
                     <div
-                        className="good-balance"
+                        className="approval-check"
                         onClick={() => {
-                            deposit();
+                            approve();
                         }}
                     >
-                        Deposit to account
+                        Approve spending
                     </div>
-                )}
-            </div>
+                </div>
+            )}
         </div>
     );
 };
